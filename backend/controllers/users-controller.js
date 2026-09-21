77873558519a33d534a0f198e7ac4b1ba84a60b2
eishsiddiqui <eishsiddiqui@gmail.com>
2026-09-21 11:35:12 +0500

    feat-User Schema & db logic added

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -1,85 +1,69 @@
 const { v4: uuidv4 } = require("uuid");
-
+const User = require("../models/user");
 const HttpError = require("../models/http-error");
 const { validationResult } = require("express-validator");
 
-const DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "Max Schwarz",
-    email: "test@test.com",
-    password: "password123",
-  },
-  {
-    id: "u2",
-    name: "Sarah Connor",
-    email: "sarah@example.com",
-    password: "sarah123",
-  },
-  {
-    id: "u3",
-    name: "John Smith",
-    email: "john@example.com",
-    password: "john123",
-  },
-  {
-    id: "u4",
-    name: "Emily Johnson",
-    email: "emily@example.com",
-    password: "emily123",
-  },
-  {
-    id: "u5",
-    name: "Ali Hassan",
-    email: "ali@example.com",
-    password: "ali123",
-  },
-  {
-    id: "u6",
-    name: "Sophia Williams",
-    email: "sophia@example.com",
-    password: "sophia123",
-  },
-  {
-    id: "u7",
-    name: "David Brown",
-    email: "david@example.com",
-    password: "david123",
-  },
-];
+async function getUsers(req, res, next) {
+  let users;
+  try {
+    users = await User.find({}, "-password");
+  } catch (err) {
+    return next(new HttpError("Error fetching Users!", 500));
+  }
 
-function getUsers(req, res, next) {
-  res.json({ users: DUMMY_USERS });
+  res.json({ users });
 }
 
-function signup(req, res, next) {
+async function signup(req, res, next) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new HttpError("Invalid inputs passed, please check your data.", 422);
   }
   const { name, email, password } = req.body;
 
-  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+  let hasUser;
 
-  if (hasUser) {
-    throw new HttpError("Emailwith this user already exists!", 422);
+  try {
+    hasUser = await User.findOne({ email: email });
+  } catch (err) {
+    console.log(err);
+    return next(new HttpError("Signup failed.Try Again!", 500));
   }
 
-  const createdUser = {
-    id: uuidv4(),
+  if (hasUser) {
+    throw new HttpError("User with this email already exists!", 422);
+  }
+
+  const newUser = new User({
     name,
     email,
+    image:
+      "https://en.wikipedia.org/wiki/Great_Wall_of_China#/media/File:The_Great_Wall_of_China_at_Jinshanling-edit.jpg",
     password,
-  };
+    places: [],
+  });
 
-  DUMMY_USERS.push(createdUser);
-  res.status(201).json({ user: createdUser });
+  try {
+    await newUser.save();
+  } catch (err) {
+    const error = new HttpError("Signing Up failed. Please try again!", 500);
+    return next(error);
+  }
+
+  res.status(201).json({ user: newUser });
 }
 
-function login(req, res, next) {
+async function login(req, res, next) {
   const { email, password } = req.body;
 
-  const registeredUser = DUMMY_USERS.find((u) => u.email === email);
+  let registeredUser;
+  try {
+    registeredUser = await User.findOne({ email: email });
+  } catch (err) {
+    console.log(err);
+    return next(new HttpError("Logging In failed.Try Again!", 500));
+  }
+
   if (!registeredUser || registeredUser.password !== password) {
     throw new HttpError(
       "Could not identify user, credentials seem to be wrong",
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
+const uniqueValidator = require("mongoose-unique-validator").default;
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: Number, required: true },
-  email: { type: Number, required: true, unique: true },
-  password: { type: Number, required: true, minlength: 6 },
-  image: { type: Number, required: true },
-  places: { type: Number, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
+  places: [{ type: mongoose.Types.ObjectId, required: true, ref: "Place" }],
 });
 
 userSchema.plugin(uniqueValidator);
